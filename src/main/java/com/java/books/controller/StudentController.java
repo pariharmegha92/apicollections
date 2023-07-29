97db37f07f306f8924ef128882458ad64558484b
pariharmegha92 <pariharmegha92@gmail.com>
2023-07-29 16:38:15 +0530

    write an RequestMapping annotation in class level

--- src/main/java/com/java/books/controller/StudentController.java
+++ src/main/java/com/java/books/controller/StudentController.java
@@ -4,10 +4,12 @@ import com.java.books.services.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/student")
 public class StudentController {
     @Autowired
     StudentService studentService;
